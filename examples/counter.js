15257420ebc936ab64ef6a1bcc68bf5d705e0719
Mikhail Chernorutsky <mikhail.chernorutsky@ontarget-group.com>
2021-06-27 00:46:54 +0300

    first commit

---
+++ examples/counter.js
@@ -0,0 +1,42 @@
+component(document.getElementById("counter"), () => {
+  let counter = 0;
+  const latestValues = [];
+  const maxLatestValues = 5;
+
+  function updateLatestValues(cnt) {
+    latestValues.push(cnt);
+    if (latestValues.length > maxLatestValues) {
+      latestValues.shift();
+    }
+  }
+
+  return () => [
+    { h1: "Counter" },
+    { span: "Current value of counter: " },
+    { span: { strong: `${counter}` } },
+    {
+      div: [
+        counter < 10 && {
+          "button.btn.increment": "increment",
+          on: {
+            onclick: () => {
+              counter++;
+              updateLatestValues(counter);
+            },
+          },
+        },
+        counter > 0 && {
+          "button.btn.decrement": "decrement",
+          on: {
+            onclick: () => {
+              counter--;
+              updateLatestValues(counter);
+            },
+          },
+        },
+      ],
+    },
+    `Latest Values (${latestValues.length}):`,
+    { div: latestValues.map((value) => ({ "span.latest": `${value}` })) },
+  ];
+});
---
+++ examples/table.js
@@ -0,0 +1,102 @@
+component(document.getElementById("table"), () => {
+  const header = ["First Name", "Last Name", "Phone", "Actions"];
+  const body = [["Ivan", "Petrov", "+18471111111"]];
+
+  let firstName = "";
+  let lastName = "";
+  let phone = "";
+
+  return () => [
+    { h1: `Employee Table - ${body.length} employees` },
+    {
+      table: [
+        { thead: { tr: header.map((item) => ({ th: item })) } },
+        {
+          tbody: body.map((row, index) => ({
+            tr: [
+              ...row.map((cell) => ({ td: cell })),
+              {
+                button: "remove",
+                on: {
+                  onclick: () => body.splice(index, 1),
+                },
+              },
+            ],
+          })),
+        },
+      ],
+    },
+    {
+      form: [
+        {
+          "div.table-row": [
+            { span: "First Name:" },
+            {
+              input: "",
+              attr: {
+                type: "text",
+                name: "first-name",
+                value: firstName,
+              },
+              on: {
+                oninput: (e) => {
+                  firstName = e.target.value;
+                },
+              },
+            },
+          ],
+        },
+        {
+          "div.table-row": [
+            { span: "Last Name:" },
+            {
+              input: "",
+              attr: {
+                type: "text",
+                name: "last-name",
+                value: lastName,
+              },
+              on: {
+                oninput: (e) => {
+                  lastName = e.target.value;
+                },
+              },
+            },
+          ],
+        },
+        {
+          "div.table-row": [
+            { span: "Phone:" },
+            {
+              input: "",
+              attr: {
+                type: "text",
+                name: "phone",
+                value: phone,
+              },
+              on: {
+                oninput: (e) => {
+                  phone = e.target.value;
+                },
+              },
+            },
+          ],
+        },
+        {
+          div: {
+            button: "Add",
+            on: {
+              onclick: (e) => {
+                e.preventDefault();
+                body.push([firstName, lastName, phone]);
+                firstName = "";
+                lastName = "";
+                phone = "";
+              },
+            },
+          },
+        },
+      ],
+    },
+  ];
+});

2e4a5031784ea12bad2705a6c58b3648cec58002
Mikhail Chernorutsky <mikhail.chernorutsky@ontarget-group.com>
2021-07-15 23:06:17 +0300

    brief mounting point

--- examples/mount.js
+++ examples/mount.js
@@ -1,19 +1,19 @@
-counter(document.getElementById("counter"), { title: "Counter 1" });
-counter(document.getElementById("counter2"), { title: "Counter 2" });
+counter("#counter", { title: "Counter 1" });
+counter("#counter2", { title: "Counter 2" });
 
-table(document.getElementById("table"), {
+table("#table", {
   header: ["First Name", "Last Name", "Phone", "Actions"],
   body: [["Ivan", "Petrov", "+18471111111"]],
 });
 
-tabs(document.getElementById("tabs"), {
+tabs("#tabs", {
   tabs: ["Tab1", "Tab2", "Tab3"],
   tabsContent: ["Content of tab1", "Content of tab2", "Content of tab3"],
 });
 
-radio(document.getElementById("radio"), {
+radio("#radio", {
   name: "lang",
   items: ["english", "spanish", "german", "chineese", "italian", "japan"],
 });
 
-clock(document.getElementById("clock"));
+clock("#clock");
